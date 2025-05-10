6e218d3d672ac18200db9f0e5a7ae49901213c14
dwyertyrell <Dwyertyrell@gmail.com>
2025-05-10 14:17:11 +0100

    Updated configuration file for Jest
    -**`jest.config.cjs`:** Added and enabled collectCoverage to see full percentage test coverage for all files
    
    -**`.gitignore`:** upon testing the full coverage, a coverage folder was created and added to `.gitignore`

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,18 @@
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}",
+    "!src/**/*.test.{js,jsx}", // Exclude test files
+    "!src/index.js", // Exclude specific files
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   testEnvironment: 'jest-environment-jsdom',
   transform: {
     '^.+\\.jsx?$': 'babel-jest', // Transform JavaScript and JSX files using Babel
