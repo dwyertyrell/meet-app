8ef660d62ee2c16506a759da368c64c8c18ef249
dwyertyrell <Dwyertyrell@gmail.com>
2025-05-17 16:37:14 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -6,54 +6,25 @@ export const extractLocations = (events) => {
   return locations;
 }
 
-const checkToken = async(accessToken) => {
-  const response = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
-  const result = await response.json();
-  return result;
-}
-
-//this function will be imported into 'CitySearchTest.js'.
-// when called, it will fetch the array in 'mockData.js' file  
-export const getEvents = async () => {
-  if(window.location.href.startsWith('http://localhost')) {
-  return mockData;
-}
-
-const removeQuery = () => {
-  let newurl;
-  if(window.history.pushState && window.location.pathname) {
-    newurl = 
-      window.location.protocol + '//' + window.location.host + window.location.pathname;
-      window.history.pushState('','', newurl); 
-  } else {
-    newurl = window.location.protocol + '//' + window.location.host;
-    window.history.pushState('','', newurl);
-  }
-}
-
-const getToken = async (code) => {
-  try{
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch('https://b4hkyo4ke7.execute-api.eu-west-2.amazonaws.com/dev/api/token' + '/' + encodeCode);
-  
-  if(!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`)
-  }
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem('access_token', access_token);
-
-  return access_token;  
-  } catch (error) {
-    error.json();
-  }
+const checkToken = async (accessToken) => {
+ const response = await fetch(
+   `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+ );
+ const result = await response.json();
+ return result;
 };
+
+
+export const getEvents = async () => {
+ if (window.location.href.startsWith('http://localhost')) {
+   return mockData;
+ }
 
 const token = await getAccessToken();
 
   if(token) {
     removeQuery();
-    const url = 'https://b4hkyo4ke7.execute-api.eu-west-2.amazonaws.com/dev/api/get-events/{access_token}' + '/' + token;
+    const url = 'https://b4hkyo4ke7.execute-api.eu-west-2.amazonaws.com/dev/api/get-events' + '/' + token;
     const response = await fetch(url);
     const result = await response.json();
 
@@ -63,25 +34,59 @@ const token = await getAccessToken();
   }
 };
 
+const removeQuery = () => {
+ let newurl;
+ if (window.history.pushState && window.location.pathname) {
+   newurl =
+     window.location.protocol +
+     "//" +
+     window.location.host +
+     window.location.pathname;
+   window.history.pushState("", "", newurl);
+ } else {
+   newurl = window.location.protocol + "//" + window.location.host;
+   window.history.pushState("", "", newurl);
+ }
+};
+
+const getToken = async (code) => {
+ const encodeCode = encodeURIComponent(code);
+ const response = await fetch(
+    'https://b4hkyo4ke7.execute-api.eu-west-2.amazonaws.com/dev/api/token' + '/' + encodeCode
+ );
+ const { access_token } = await response.json();
+ access_token && localStorage.setItem("access_token", access_token);
+
+
+ return access_token;
+};
+
+
+
+
+
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
 
-  const tokenCheck = accessToken && (await checkToken(accessToken));
+ const tokenCheck = accessToken && (await checkToken(accessToken));
 
-  if(!accessToken || tokenCheck.error) {
-    await localStorage.removeItem('access_token');
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get('code');
-    if(!code) {
-      const response = await fetch(
-        'https://b4hkyo4ke7.execute-api.eu-west-2.amazonaws.com/dev/api/get-auth-url');
-      
-      const result = await response.json();
-      const {authUrl} = result;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return accessToken
+
+ if (!accessToken || tokenCheck.error) {
+   await localStorage.removeItem("access_token");
+   const searchParams = new URLSearchParams(window.location.search);
+   const code = await searchParams.get("code");
+   if (!code) {
+     const response = await fetch(
+ 'https://b4hkyo4ke7.execute-api.eu-west-2.amazonaws.com/dev/api/get-auth-url'
+     );
+     const result = await response.json();
+     const { authUrl } = result;
+     return (window.location.href = authUrl);
+   }
+   return code && getToken(code);
+ }
+ return accessToken;
 }
+
+
 
