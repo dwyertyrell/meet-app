c03e780933516cffdbb0cb289516f65109ae82ec
dwyertyrell <Dwyertyrell@gmail.com>
2025-05-29 14:24:27 +0100

    feat(APM): implemented an Application Performance Monitor
    installed  "atatus-spa" package to provide a browser-side React monitoring to track how users experience the React app

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './App.css'
+import * as atatus from 'atatus-spa';
+
+atatus.config('e2fd804f85cc40b380980382d6bdc618').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+atatus.notify(new Error('Test Atatus Setup'));
